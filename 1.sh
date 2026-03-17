e75b5f4ee64e29e7bfe69f9e67d70d685af7c075
stepanovanastya1104 <117353840+stepanovanastya1104@users.noreply.github.com>
2026-03-17 21:16:32 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -7,23 +7,38 @@ echo "Delete all hard links (synonyms) of FILE inside DIR and its subdirectories
 echo
 echo "Options:"
 echo "  -h, --help     show this help message and exit"
+echo "  -d, --directory DIR     path to the target directory"
+echo "  -f, --file FILE         target file"
 echo "Exit status:"
 echo " 0   if OK,"
 echo " 1   if invalid arguments or missing parameters"
 echo " 2   if FILE does not exist"
 echo " 3   if DIR does not exist or is not a directory"
 }
-
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
     help
     exit 0
 fi
-if [ $# -ne 2 ]; then
+if [[ "$1" == "-d" || "$1" == "--directory" ]]; then
+    dir="$2"
+    if [[ "$3" == "-f" || "$3" == "--file" ]]; then
+        file="$4"
+    else
+      help
+      exit 1
+    fi
+elif [[ "$1" == "-f" || "$1" == "--file" ]]; then
+    file="$2"
+    if [[ "$3" == "-d" || "$3" == "--directory" ]]; then
+        dir="$4"
+    else
+      help
+      exit 1
+    fi
+else
     help
     exit 1
 fi
-file=$1
-dir=$2
 if [ ! -f "$file" ]; then
     echo "Error: file '$file' does not exist"
     exit 2
